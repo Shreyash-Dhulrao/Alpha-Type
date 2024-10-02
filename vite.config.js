a93c0d2412a30bf5c9a8ee8e215c97bb2dde0404
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-10-02 12:23:51 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import main from './src/main'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react() , svgr()],
+  build: {
+    outDir: {main}, 
+  },
 })
